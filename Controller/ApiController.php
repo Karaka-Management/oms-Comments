de9d99d18bd35b516ae7caaeb38235e6263991bf
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-02-19 13:57:38 +0100

    fix name

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Orange Management
+ * Karaka
  *
  * PHP Version 8.0
  *
@@ -9,7 +9,7 @@
  * @copyright Dennis Eichhorn
  * @license   OMS License 1.0
  * @version   1.0.0
- * @link      https://orange-management.org
+ * @link      https://karaka.app
  */
 
 declare(strict_types=1);
@@ -42,7 +42,7 @@ use phpOMS\Utils\Parser\Markdown\Markdown;
  *
  * @package Modules\Comments
  * @license OMS License 1.0
- * @link    https://orange-management.org
+ * @link    https://karaka.app
  * @since   1.0.0
  */
 final class ApiController extends Controller
--- Controller/BackendController.php
+++ Controller/BackendController.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Orange Management
+ * Karaka
  *
  * PHP Version 8.0
  *
@@ -9,7 +9,7 @@
  * @copyright Dennis Eichhorn
  * @license   OMS License 1.0
  * @version   1.0.0
- * @link      https://orange-management.org
+ * @link      https://karaka.app
  */
 
 declare(strict_types=1);
@@ -31,7 +31,7 @@ use phpOMS\Views\View;
  *
  * @package Modules\Comments
  * @license OMS License 1.0
- * @link    https://orange-management.org
+ * @link    https://karaka.app
  * @since   1.0.0
  * @codeCoverageIgnore
  */
