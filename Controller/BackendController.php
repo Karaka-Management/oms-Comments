03cc6492317af0be31f61ea15e35c9a83a6b536b
Dennis Eichhorn <spl1nes.com@googlemail.com>
2024-05-21 00:09:08 +0200

    fix version and bugs

--- Controller/BackendController.php
+++ Controller/BackendController.php
@@ -7,7 +7,7 @@
  *
  * @package   Modules\Comments
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -28,7 +28,7 @@ use phpOMS\Views\View;
  * Calendar controller class.
  *
  * @package Modules\Comments
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  * @codeCoverageIgnore
--- Models/CommentVote.php
+++ Models/CommentVote.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\Comments\Models
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -20,7 +20,7 @@ use Modules\Admin\Models\Account;
  * Comment vote class.
  *
  * @package Modules\Comments\Models
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  */
